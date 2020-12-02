4072c7c5fe6bdd11c5174d6b3a7c8fa96aed5428
qui16000158 <55698561+qui16000158@users.noreply.github.com>
2020-12-02 15:46:43 +0000

    Added tilt controls

---
+++ Assets/Scripts/Input/MobileInput.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileInput : InputBase, IInput
+{
+    public float GetAcceleration()
+    {
+        return input.Default.Action.ReadValue<float>();
+    }
+
+    public float GetHorizontal()
+    {
+        return input.Default.Horizontal.ReadValue<float>();
+    }
+}
