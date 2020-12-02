8b48aa8e0f866c40186ead6cf8b99840b186c4bd
Twilit <37117133+Twilit@users.noreply.github.com>
2020-12-02 01:13:41 +0000

    Basic timer implemented

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    private int initialTimer = 3;
+    public int currentTimer;
+    public Text timerDisplayText;
+
+    void Start()
+    {
+        StartCoroutine(Countdown());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Countdown()
+    {
+        currentTimer = initialTimer;
+        timerDisplayText.text = "You have " + initialTimer + " seconds";
+
+        while (currentTimer > 0)
+        {
+            yield return new WaitForSeconds(1);
+            currentTimer--;
+            timerDisplayText.text = "Time remaining: " + currentTimer + " seconds";
+        }
+    }
+}
