4c96115e0bafc6cff3d3c47aa21834a9c09cd912
Silenthowler <23553428+domcrouse@users.noreply.github.com>
2020-12-09 00:25:29 +0000

    slider save test

---
+++ Assets/Slidercontroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slidercontroller : MonoBehaviour
+{
+    public Slider slider;
+
+    public float sliderVal;
+
+    public void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("save", sliderVal);
+    }
+
+    public void sliderchange(float value)
+    {
+        sliderVal = value;
+        PlayerPrefs.SetFloat("save", sliderVal);
+    }
+}
